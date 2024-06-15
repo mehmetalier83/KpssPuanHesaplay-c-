3acca664655ce5a426fd19f85bb161f8531f55f9
Mehmet Ali ER <mehmetalier83@hotmail.com>
2024-06-15 20:52:50 +0300

    fix

---
+++ Kpss-Puan-Hesaplayici/Views/SharedViews/HesaplaButton.swift
@@ -0,0 +1,20 @@
+// 
+//  HesaplaButton.swift
+//  Kpss-Puan-Hesaplayici
+//
+//  Created by Mehmet Ali Er on 15.06.2024.
+//
+
+import SwiftUI
+
+struct HesaplaButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HesaplaButton_Previews: PreviewProvider {
+    static var previews: some View {
+        HesaplaButton()
+    }
+}
